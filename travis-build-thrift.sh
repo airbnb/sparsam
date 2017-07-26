bf5ef642b4aaaf060b49ee13d26ebfd7cfe5ac81
Dezhi (Andy) Fang <dezhi.fang@airbnb.com>
2017-07-26 13:12:33 -0700

    Add ldconfig

--- travis-build-thrift.sh
+++ travis-build-thrift.sh
@@ -3,3 +3,4 @@ tar xvf thrift.tar.gz
 cd thrift-0.10.0 
 cmake . -DBUILD_COMPILER=OFF -DBUILD_C_GLIB=OFF -DBUILD_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_JAVA=OFF 
 make -j && sudo make install
+sudo ldconfig
