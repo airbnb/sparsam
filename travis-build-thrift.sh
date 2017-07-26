4efc84337c84bfc9e0469873ef126305819da643
Dezhi (Andy) Fang <dezhi.fang@airbnb.com>
2017-07-26 13:05:54 -0700

    Separate out the file for building thrift in travis

---
+++ travis-build-thrift.sh
@@ -0,0 +1,5 @@
+cd /tmp 
+tar xvf thrift.tar.gz 
+cd thrift-0.10.0 
+cmake . -DBUILD_COMPILER=OFF -DBUILD_C_GLIB=OFF -DBUILD_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_JAVA=OFF 
+make -j && sudo make install
