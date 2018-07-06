42c316a40eaa633efb50e2d114519841ce9f7a8d
Andy Fang <andy.fang@airbnb.com>
2018-07-06 16:35:55 -0700

    Additional logic to handle naked unions

--- lib/sparsam/deserializer.rb
+++ lib/sparsam/deserializer.rb
@@ -2,7 +2,11 @@ module Sparsam
   module Deserializer
     def self.deserialize(klass, serialized_string, prot = Sparsam::CompactProtocol)
       s = Sparsam::Serializer.new(prot, serialized_string)
-      s.deserialize(klass)
+      if klass <= Sparsam::Union
+         s.deserializeUnion(klass)
+      else
+        s.deserialize(klass)
+      end
     end
   end
 end
