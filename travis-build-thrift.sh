f008f56087a872293190942efbb46fd7c557cfc5
Dezhi “Andy” Fang <andyfang.dz@gmail.com>
2018-07-06 19:52:52 -0700

    Switch to a non-sudo travis build (#19)
    * First pass of non-sudo travis
    
    * use global

--- travis-build-thrift.sh
+++ travis-build-thrift.sh
@@ -1,6 +1,17 @@
-cd /tmp 
-tar xvf thrift.tar.gz 
-cd thrift-0.10.0 
-cmake . -DBUILD_COMPILER=OFF -DBUILD_C_GLIB=OFF -DBUILD_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_JAVA=OFF 
-make -j && sudo make install
-sudo ldconfig
+#!/usr/bin/env bash
+set -euxo pipefail
+
+mkdir -p $HOME/thrift
+cd $HOME/thrift
+
+echo "$THRIFT_SHA256SUM" > SHA256SUMS
+ret=0
+sha256sum -c SHA256SUMS 2>&1 || ret=$?
+
+if [ $ret -ne 0 ]; then
+    wget -N "$THRIFT_URL"
+    tar xvf "$THRIFT_FILE"
+    cd "$THRIFT_DIR"
+    cmake . -DBUILD_COMPILER=OFF -DBUILD_C_GLIB=OFF -DBUILD_PYTHON=OFF -DBUILD_TESTING=OFF -DBUILD_JAVA=OFF
+    make -j
+fi
