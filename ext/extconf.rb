b505390821c8edefe22cd900ac9f82452b727ae9
Gregory Szorc <gregory.szorc@airbnb.com>
2022-01-13 11:48:05 -0800

    More robust extension configuration
    Previously, `extconf.rb` was rather simple and blindly trusted that
    things would work. This commit drastically changes the situation.
    
    `extconf.rb` now errors hard if `have_library('thrift')` does not
    work. This should short-circuit build/compile time errors that would
    later occur at build time due to a missing/wrong thrift configuration.
    
    But the bigger change is the introduction of new functionality to
    sniff for and automatically use packages provided by Homebrew.
    
    Essentially, if the `brew` executable is found, we call
    `brew --prefix thrift@0.9` and `brew --prefix boost` to resolve
    and automatically use include and library paths under these paths,
    if available.
    
    If the `thrift@0.9` and `boost` Homebrew packages are installed,
    the extension configuration should "just work." I've tested this on
    both an x86-64 and M1 device using macOS Monterey.
    
    Because not all users may desire the automatic Homebrew usage or
    its choice of packages to use, configuration options to disable
    the functionality or point at different Homebrew packages have been
    added and documented.
    
    `extconf.rb` is also a bit more chattier now to help debug issues.
    Failures during Homebrew integration are more actionable to help users
    self-diagnose.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -7,12 +7,70 @@ if Gem::Version.new(RUBY_VERSION.dup) < Gem::Version.new('2.0.0')
   $CPPFLAGS += $CXXFLAGS
 end
 
+brew = find_executable('brew')
+
+# Automatically use homebrew to discover thrift package if it is
+# available and not disabled via --disable-homebrew.
+use_homebrew = enable_config('homebrew', brew)
+
+if use_homebrew
+  $stderr.puts "Using #{brew} to locate thrift and boost packages"
+  $stderr.puts '(disable Homebrew integration via --disable-homebrew)'
+
+  thrift_package = with_config('homebrew-thrift-package', 'thrift@0.9')
+  $stderr.puts "looking for Homebrew thrift package '#{thrift_package}'"
+  $stderr.puts '(change Homebrew thrift package name via --with-homebrew-thrift-package=<package>)'
+
+  thrift_prefix = `#{brew} --prefix #{thrift_package}`.strip
+
+  unless File.exists?(thrift_prefix)
+    $stderr.puts "#{thrift_prefix} does not exist"
+    $stderr.puts "To resolve, `brew install #{thrift_package}` or pass"
+    $stderr.puts '--with-homebrew-thrift-package=<package> to this build to specify'
+    $stderr.puts 'an alternative thrift package to build against. e.g.:'
+    $stderr.puts
+    $stderr.puts '  $ bundle config --global build.sparsam --with-homebrew-thrift-package=mythrift'
+
+    raise "Homebrew package #{thrift_package} not installed"
+  end
+
+  $stderr.puts "using Homebrew thrift at #{thrift_prefix}"
+  $CFLAGS << " -I#{thrift_prefix}/include"
+  $CXXFLAGS << " -I#{thrift_prefix}/include"
+  $CPPFLAGS << " -I#{thrift_prefix}/include"
+  $LDFLAGS << " -L#{thrift_prefix}/lib"
+
+  # Also add boost to the includes search path if it is installed.
+  boost_package = with_config('homebrew-boost-package', 'boost')
+  $stderr.puts "looking for Homebrew boost package '#{boost_package}'"
+  $stderr.puts '(change Homebrew boost package name via --with-homebrew-boost-package=<package>)'
+
+  boost_prefix = `#{brew} --prefix #{boost_package}`.strip
+
+  if File.exists?(boost_prefix)
+    $stderr.puts("using Homebrew boost at #{boost_prefix}")
+    $CFLAGS << " -I#{boost_prefix}/include"
+    $CXXFLAGS << " -I#{boost_prefix}/include"
+    $CPPFLAGS << " -I#{boost_prefix}/include"
+  else
+    $stderr.puts 'Homebrew boost not found; assuming boost is in default search paths'
+  end
+end
+
 have_func("strlcpy", "string.h")
-have_library("thrift")
+
+unless have_library("thrift")
+  raise 'thrift library not found; aborting since compile would fail'
+end
+
 libs = ['-lthrift']
 
 libs.each do |lib|
   $LOCAL_LIBS << "#{lib} "
 end
+
+# Ideally we'd validate boost as well. But boost is header only and
+# mkmf's have_header() doesn't work with C++ headers.
+# https://bugs.ruby-lang.org/issues/4924
 
 create_makefile 'sparsam_native'
